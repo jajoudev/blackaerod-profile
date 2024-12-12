58deefeda16c279ea203b91ef3fd39a97c4ad390
JajouDev <jahelgithub64@gmail.com>
2024-12-12 11:43:18 +0100

    Glowing text

--- src/components/TypewriterEffect/Typewriter.jsx
+++ src/components/TypewriterEffect/Typewriter.jsx
@@ -2,7 +2,7 @@ import TypewriterComponent from "typewriter-effect";
 
 function Typewriter() {
   return (
-    <div className="font-comic font-extrabold text-3xl">
+    <div className="font-comic font-extrabold text-3xl [text-shadow:0px_0px_43px_rgba(255,174,0,1)]">
         <TypewriterComponent
           options={{
             strings: ["Blackaerod", "@blackaerod94", "blacknakito"],
